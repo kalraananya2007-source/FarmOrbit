d72c0d9c78324b856ff5ef828e3d82ab38467877
gauri <gauribajaj010@gmail.com>
2026-08-29 22:07:46 +0530

    Integrated weather page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Weather from './weather/weather'
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Weather/>
       <Footer />
     </>
   );
