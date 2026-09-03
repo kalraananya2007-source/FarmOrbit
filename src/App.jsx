2680d87338c96274a3284022799f64df992ba80e
Nishi <nishi05012008@gmail.com>
2026-09-03 17:39:03 +0530

    inproved implementation and fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,74 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
-import Weather from './weather/weather'
+import ComingSoon from './pages/ComingSoon';
+
+import Farmer from './components/farmer';
+import Weather from './weather/weather';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <Weather/>
+
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Farmer Module */}
+        <Route path="/farmer" element={<Farmer />} />
+
+        {/* Weather Module */}
+        <Route path="/weather" element={<Weather />} />
+
+        {/* Marketplace - Coming Soon for now */}
+        <Route
+          path="/marketplace"
+          element={
+            <ComingSoon
+              title="Marketplace"
+              message="Marketplace feature is coming soon."
+            />
+          }
+        />
+
+        {/* Login - Coming Soon */}
+        <Route
+          path="/login"
+          element={
+            <ComingSoon
+              title="Login"
+              message="Login feature is coming soon."
+            />
+          }
+        />
+
+        {/* Register - Coming Soon */}
+        <Route
+          path="/register"
+          element={
+            <ComingSoon
+              title="Register"
+              message="Registration feature is coming soon."
+            />
+          }
+        />
+
+        {/* Any wrong URL */}
+        <Route
+          path="*"
+          element={
+            <ComingSoon
+              title="Page Not Found"
+              message="The page you are looking for does not exist."
+            />
+          }
+        />
+      </Routes>
+
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,8 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
+
+        {/* Logo */}
         <Link to="/" className="navbar-logo">
           🌱 FarmOrbit
         </Link>
@@ -22,15 +24,72 @@ function Navbar() {
           {menuOpen ? '✕' : '☰'}
         </div>
 
-        <ul className={menuOpen ? 'navbar-links active' : 'navbar-links'}>
-          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/farmer" onClick={() => setMenuOpen(false)}>Farmer Module</Link></li>
-          <li><Link to="/marketplace" onClick={() => setMenuOpen(false)}>Marketplace</Link></li>
+        {/* Navigation Links */}
+        <ul
+          className={
+            menuOpen ? 'navbar-links active' : 'navbar-links'
+          }
+        >
+          {/* Home */}
+          <li>
+            <Link
+              to="/"
+              onClick={() => setMenuOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+
+          {/* Farmer Module */}
+          <li>
+            <Link
+              to="/farmer"
+              onClick={() => setMenuOpen(false)}
+            >
+              Farmer Module
+            </Link>
+          </li>
+
+          {/* Weather */}
+          <li>
+            <Link
+              to="/weather"
+              onClick={() => setMenuOpen(false)}
+            >
+              Weather
+            </Link>
+          </li>
+
+          {/* Marketplace */}
+          <li>
+            <Link
+              to="/marketplace"
+              onClick={() => setMenuOpen(false)}
+            >
+              Marketplace
+            </Link>
+          </li>
+
+          {/* Login & Register */}
           <li className="navbar-buttons">
-            <Link to="/login" className="btn btn-outline">Login</Link>
-            <Link to="/register" className="btn btn-primary">Register</Link>
+            <Link
+              to="/login"
+              className="btn btn-outline"
+              onClick={() => setMenuOpen(false)}
+            >
+              Login
+            </Link>
+
+            <Link
+              to="/register"
+              className="btn btn-primary"
+              onClick={() => setMenuOpen(false)}
+            >
+              Register
+            </Link>
           </li>
         </ul>
+
       </div>
     </nav>
   );
