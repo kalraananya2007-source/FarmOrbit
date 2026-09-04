27d952c2488d557ad44b28c6ea2ba03a2a3dc26c
Bhavya2737 <bhavyaynr07@gmail.com>
2026-09-04 21:34:02 +0530

    Add route for crop management

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Weather from './weather/weather'
+import AddCrop from './pages/AddCrop';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-crop" element={<AddCrop />} />
+      </Routes>
       <Weather/>
       <Footer />
     </>
